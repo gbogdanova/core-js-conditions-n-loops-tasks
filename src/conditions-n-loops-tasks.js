fb01fd1e053570194ed86c88b32f6a148b5e9597
Galya Bogdanova <galya_bogdanova@MacBook-Air-Maksim.local>
2024-01-06 13:37:27 -0800

    feat: implemented isPalindrome, getIndexOf, isContainNumber functions

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -222,8 +222,14 @@ function convertNumberToString(numberStr) {
  *  '0123210'   => true
  *  'qweqwe'    => false
  */
-function isPalindrome(/* str */) {
-  throw new Error('Not implemented');
+function isPalindrome(str) {
+  let strRevert = '';
+
+  for (let i = str.length - 1; i >= 0; i -= 1) {
+    strRevert += str[i];
+  }
+
+  return strRevert === str;
 }
 
 /**
@@ -240,8 +246,13 @@ function isPalindrome(/* str */) {
  *  'qwerty', 'Q'     => -1
  *  'qwerty', 'p'     => -1
  */
-function getIndexOf(/* str, letter */) {
-  throw new Error('Not implemented');
+function getIndexOf(str, letter) {
+  for (let i = 0; i < str.length; i += 1) {
+    if (str[i] === letter) {
+      return i;
+    }
+  }
+  return -1;
 }
 
 /**
@@ -259,8 +270,15 @@ function getIndexOf(/* str, letter */) {
  *  12345, 0    => false
  *  12345, 6    => false
  */
-function isContainNumber(/* num, digit */) {
-  throw new Error('Not implemented');
+function isContainNumber(num, digit) {
+  let numCopy = num;
+  while (numCopy > 0) {
+    if (numCopy % 10 === digit) {
+      return true;
+    }
+    numCopy = Math.floor(numCopy / 10);
+  }
+  return false;
 }
 
 /**
